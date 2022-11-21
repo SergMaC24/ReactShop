5f93ce76a99885f3cbd00f4414b729e3901f12ef
SergMac24 <serg808507@gmail.com>
2022-11-21 11:13:44 +0200

    first commit

---
+++ client/src/components/BrandBar.js
@@ -0,0 +1,22 @@
+import React, {useContext} from 'react';
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import {Card, Form, Row} from "react-bootstrap";
+
+const BrandBar = observer(() => {
+    const {device} = useContext(Context)
+    return (
+        <Form className="d-flex">
+            {device.brands.map(brand =>
+                <Card
+                    key={brand.id}
+                    className="p-3"
+                >
+                    {brand.name}
+                </Card>
+            )}
+        </Form>
+    );
+});
+
+export default BrandBar;
---
+++ client/src/store/DeviceStore.js
@@ -0,0 +1,51 @@
+import {makeAutoObservable} from "mobx";
+
+export default class DeviceStore {
+    constructor() {
+        this._types = [
+            {id: 1, name: 'Холодильники'},
+            {id: 2, name: 'Смартфоны'},
+            {id: 3, name: 'Nike'},
+            {id: 4, name: 'Puma'},
+        ]
+        this._brands = [
+            {id:1, name: 'Apple'},
+            {id:2, name: 'Samsung'},
+        ]
+        this._devices = [
+            {id: 1, name: 'Iphone 12 pro', price: 2500, rating: 5, img: 'https://m.media-amazon.com/images/I/611fy2og0QL._AC_SY606_.jpg'},
+            {id: 1, name: 'Iphone 12 pro', price: 2500, rating: 5, img: 'https://m.media-amazon.com/images/I/611fy2og0QL._AC_SY606_.jpg'},
+            {id: 1, name: 'Iphone 12 pro', price: 2500, rating: 5, img: 'https://m.media-amazon.com/images/I/611fy2og0QL._AC_SY606_.jpg'},
+            {id: 1, name: 'Iphone 12 pro', price: 2500, rating: 5, img: 'https://m.media-amazon.com/images/I/611fy2og0QL._AC_SY606_.jpg'},
+            {id: 1, name: 'Iphone 12 pro', price: 2500, rating: 5, img: 'https://m.media-amazon.com/images/I/611fy2og0QL._AC_SL1500_.jpg'},
+        ]
+        this._selectedType = {}
+        makeAutoObservable(this)
+    }
+
+    setTypes(types) {
+        this._types = types
+    }
+    setBrands(brands) {
+        this._brands = brands
+    }
+    setDevices(devices) {
+        this._devices = devices
+    }
+    setSelectedType(type) {
+        this._selectedType = type
+    }
+
+    get types() {
+        return this._types
+    }
+    get brands() {
+        return this._brands
+    }
+    get devices() {
+        return this._devices
+    }
+    get selectedType() {
+        return this._selectedType
+    }
+}
